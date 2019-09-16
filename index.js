f8fc8988965b3a69928fcf69a282e296f8902ba5
lav <l.venier@yoctu.com>
2019-09-15 20:12:46 -0400

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.use('/discord', require('./discord'));
+
+app.get('/game', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'game/index.html'));
+});
+
+app.get('/', (req, res) => {
+	res.redirect("/discord/login");
+});
+
+app.listen(8000, () => {
+});
---
+++ public/main.js
@@ -0,0 +1,27 @@
+// Global Variables
+var
+  game = new Phaser.Game(800, 600, Phaser.AUTO, 'game'),
+  Main = function () {},
+  gameOptions = {
+    playSound: true,
+    playMusic: true
+  };
+
+Main.prototype = {
+
+  preload: function () {
+    game.load.image('stars',    'assets/images/stars.jpg');
+    game.load.image('loading',  'assets/images/loading.png');
+    game.load.image('brand',    'assets/images/logo.png');
+    game.load.script('splash',  'states/Splash.js');
+  },
+
+  create: function () {
+    game.state.add('Splash', Splash);
+    game.state.start('Splash');
+  }
+
+};
+
+game.state.add('Main', Main);
+game.state.start('Main');

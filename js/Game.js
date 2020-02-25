fab858aa7cde1487828d0fbdc11e7db146a0cd27
lav <l.venier@yoctu.com>
2020-02-24 22:17:11 -0500

    add floor texture

--- js/Game.js
+++ js/Game.js
@@ -64,8 +64,12 @@ function init() {
 
     var floorGeometry = new THREE.PlaneBufferGeometry(5000, 5000, 100, 100);
     floorGeometry.rotateX(-Math.PI / 2);
+    var floorTexture = new THREE.TextureLoader().load("textures/minecraft/textures/block/green_wool.png");
+    floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+    floorTexture.repeat.set(100, 100);
     var floorMaterial = new THREE.MeshBasicMaterial({
-        color: "green"
+        map: floorTexture,
+        side: THREE.DoubleSide
     });
     var floor = new THREE.Mesh(floorGeometry, floorMaterial);
     floor.position.set(0, -50, 0);
